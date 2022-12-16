d5841fa5a7ded4617b69cec300c2fbbaef0f955f
John <johntony366@gmail.com>
2022-12-16 15:21:29 +0530

    styling: Fix minor styling issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -159,23 +159,30 @@ export default function Home() {
 
       <Box h="100vh" pt={"60px"} color="white" bg="rgba(3, 0, 30, 1)">
         <Flex h="100%" direction={"column"} justify={"center"} align={"center"}>
-          <Flex>
-            <Box mr={20}>
-              <Heading maxW={400} mb={12} color={"rgba(9, 124, 158, 1)"}>
+          <Flex w="100%" justifyContent={"space-around"}>
+            <Box>
+              <Heading
+                maxW={400}
+                mb={12}
+                color={"rgba(9, 124, 158, 1)"}
+                fontSize="4xl"
+              >
                 Find a circle to work, earn, and network
               </Heading>
-              <Text maxW={500}>
+              <Text maxW={500} fontSize="large" textAlign={"justify"}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.
               </Text>
             </Box>
-            <Image
-              w={300}
-              src="/images/3-circles.svg"
-              alt="3 overlapping circles"
-            ></Image>
+            <Box alignSelf={"center"}>
+              <Image
+                w={250}
+                src="/images/3-circles.svg"
+                alt="3 overlapping circles"
+              />
+            </Box>
           </Flex>
         </Flex>
       </Box>
