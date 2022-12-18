c62a086c6f241af2e0fc609ce47fe053fcd10885
John <johntony366@gmail.com>
2022-12-18 11:29:56 +0530

    Make Know more panel responsive

--- components/index/KnowMore.tsx
+++ components/index/KnowMore.tsx
@@ -3,17 +3,32 @@ import React from "react";
 
 export const KnowMore = () => {
   return (
-    <Flex h="50vh" direction={"column"} bg={"rgba(30, 30, 30, 1)"}>
+    <Flex h="75vh" direction={"column"} bg={"rgba(30, 30, 30, 1)"}>
       <Heading
         textAlign={"center"}
-        mt={8}
-        mb={16}
+        mt={12}
         color={"rgba(242, 246, 249, 1)"}
+        mx={4}
       >
         Get to know more about our platform
       </Heading>
-      <Flex justify={"center"} gap={24}>
+      <Flex justify={"center"} align={"center"} gap={24} flexGrow={1}>
         <Box
+          w={"75vw"}
+          maxW={760}
+          bg={"#d9d9d9"}
+          sx={{
+            boxShadow: "0px 20px 40px rgba(0, 0, 0, 0.1)",
+            borderRadius: "45px",
+            transform: "rotate(180deg)",
+            aspectRatio: {
+              base: "1 / 1",
+              sm: "3 / 2",
+              md: "2 / 1",
+            },
+          }}
+        ></Box>
+        {/* <Box
           w={200}
           h={200}
           bg={"#2B2B2B"}
@@ -22,27 +37,7 @@ export const KnowMore = () => {
             borderRadius: "45px",
             transform: "rotate(180deg)",
           }}
-        ></Box>
-        <Box
-          w={200}
-          h={200}
-          bg={"#2B2B2B"}
-          sx={{
-            boxShadow: "0px 20px 40px rgba(0, 0, 0, 0.1)",
-            borderRadius: "45px",
-            transform: "rotate(180deg)",
-          }}
-        ></Box>
-        <Box
-          w={200}
-          h={200}
-          bg={"#2B2B2B"}
-          sx={{
-            boxShadow: "0px 20px 40px rgba(0, 0, 0, 0.1)",
-            borderRadius: "45px",
-            transform: "rotate(180deg)",
-          }}
-        ></Box>
+        ></Box> */}
       </Flex>
     </Flex>
   );
