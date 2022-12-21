a92d33e8154c388f0754ad86d7271d9b403ef9f6
Tanmay000009 <tanmayvyas009@gmail.com>
2022-12-21 20:11:32 +0530

    add team

---
+++ components/about/TeamMember.tsx
@@ -0,0 +1,53 @@
+import { Box, Center } from "@chakra-ui/react";
+
+const TeamMember = ({
+  name,
+  title,
+  image,
+  bio,
+}: {
+  name: string;
+  title: string;
+  image: string;
+  bio: string;
+}) => {
+  return (
+    <Box
+      background={
+        "linear-gradient(225deg, rgba(255, 255, 255, 0.04) 0%, rgba(255, 255, 255, 0.04) 100%), #03001E"
+      }
+      flexBasis={{
+        base: "100%",
+        md: "20%",
+      }}
+      mx={{
+        base: "4rem",
+        md: "2rem",
+      }}
+      my={{
+        base: "2rem",
+      }}
+      p="1rem"
+    >
+      <Center>
+        <img src={image} alt={name} />
+      </Center>
+      <Box
+        m="2rem"
+        sx={{
+          background:
+            "linear-gradient(225deg, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0.1) 100%), #03001E",
+          boxShadow: "0px 20px 40px rgba(0, 0, 0, 0.1)",
+          backdropFilter: "blur(15px)",
+        }}
+        textAlign="center"
+      >
+        <Center>{name}</Center>
+        <br />
+        {bio}
+      </Box>
+    </Box>
+  );
+};
+
+export default TeamMember;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,15 @@
 import { Flex } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import AboutUs from "../components/about/About";
+import OurTeam from "../components/about/OurTeam";
 
 const About = () => {
   return (
-    <>
-      <Flex direction="column" bgColor="#03001E" minH="100vh">
-        <Navbar page="ABOUT" />
-        <AboutUs />
-      </Flex>
-    </>
+    <Flex direction="column" bgColor="#03001E" minH="100vh">
+      <Navbar page="ABOUT" />
+      <AboutUs />
+      <OurTeam />
+    </Flex>
   );
 };
 
