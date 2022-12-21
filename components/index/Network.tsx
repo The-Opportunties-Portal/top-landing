9d57aa8a3eae276bfdc153540342a5bfe6f35b7b
John <johntony366@gmail.com>
2022-12-22 00:07:03 +0530

    Update network content

--- components/index/Network.tsx
+++ components/index/Network.tsx
@@ -32,7 +32,7 @@ export const Network = () => {
             color={"rgba(9, 124, 158, 1)"}
             fontSize={{ base: "3xl", sm: "4xl" }}
           >
-            Find a circle to work, earn, and network
+            Find a circle to work, develop, and network
           </Heading>
           <Text maxW={500} fontSize={{ base: "small", sm: "medium" }}>
             The Opportunities Portal is a platform that connects skilled
@@ -40,18 +40,6 @@ export const Network = () => {
             campus. It provides a swift, direct, and transparent way for these
             groups to discover and engage with one another, potentially leading
             to new job opportunities, partnerships, or investments.
-          </Text>
-          <br />
-          <Text maxW={500} fontSize={{ base: "small", sm: "medium" }}>
-            For all students, TOP is a valuable resource for finding and
-            learning about new opportunities with startups and VCs on their
-            campus and finding potential niches and people they are looking for!
-          </Text>
-          <br />
-          <Text maxW={500} fontSize={{ base: "small", sm: "medium" }}>
-            For startups and VCs and other hiring parties, TOP is a useful tool
-            for accessing a pool of qualified candidates and calling the best
-            fit for their organization.
           </Text>
         </Box>
         <Box alignSelf={"center"}>
